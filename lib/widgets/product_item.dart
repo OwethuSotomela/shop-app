9ffa671040705a7c984916afbcc24b25b1a48107
OwethuSotomela <owethusotomela@gmail.com>
2022-03-26 18:02:17 +0200

    Using nested models and providers

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -35,8 +35,12 @@ class ProductItem extends StatelessWidget {
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
+            icon: Icon(
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+            ),
+            onPressed: () {
+              product.toggleFavoriteStatus();
+            },
             color: Theme.of(context).accentColor,
           ),
           title: Text(
